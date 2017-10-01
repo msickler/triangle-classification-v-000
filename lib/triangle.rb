b78fc11c61c3dfa9c59f6de508e8be2e637dd77e
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-01 18:41:16 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -1,23 +1,23 @@
 class Triangle
-  attr_accessor :a, :b, :c 
+  attr_accessor :a, :b, :c
 
   def initialize
-    @a = a 
-    @b = b 
-    @c = c 
+    @a = a
+    @b = b
+    @c = c
   end
 
   def kind
     if (@a + @b) <= @c || @a + @c <= @b || @b + @c <= @a || self.any? <= 0
-      raise TriangleError    
-    elsif @a == @b == @c 
-      :equilateral 
-    elsif @a == @b || @b == @c || @a == @c 
+      raise TriangleError
+    elsif @a == @b == @c
+      :equilateral
+    elsif @a == @b || @b == @c || @a == @c
       :isosceles
     else :scalene
     end
   end
-
+end
 
 end
 
