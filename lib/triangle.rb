ad69215d95840e95dab6671647562270bad94210
Mariana Sickler <marianaleal_93@hotmail.com>
2017-10-01 18:45:07 +0000

    automatically backed up by learn

--- lib/triangle.rb
+++ lib/triangle.rb
@@ -8,7 +8,7 @@ class Triangle
   end
 
   def kind
-       if (a + b) <= c || (a + c) <= b || (b + c) <= a ||
+       if (@a + @b) <= @c || (a + c) <= b || (b + c) <= a ||
        a <= 0 || b <= 0 || c <= 0
          raise TriangleError
        else
